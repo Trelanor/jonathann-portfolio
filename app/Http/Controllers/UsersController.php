225da9cd091910fc03be2f98003aee2497b77dc6
Trelanor <trelanor@gmail.com>
2018-08-02 14:03:47 +0200

    formulaire

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -13,6 +14,76 @@ class UsersController extends Controller
  
     public function store(Request $request)
     {
-        return 'Le nom est ' . $request->input('nom');
+        
+
+        $contact = new Contact();
+        $contact->nom = $request->input('nom');
+        $contact->prenom = $request->input('prenom');
+        $contact->email = $request->input('email');
+        $contact->sujet = $request->input('sujet');
+        $contact->message = $request->input('message');
+
+        $contact->save();
+
+        $this->sendEmail($contact);
+        
+    }
+
+    private function sendEmail($contact)
+    {
+        if(!empty($_POST)) {
+		
+            $errors = [];
+            
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $sujet = $_POST['sujet'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+            
+            if(empty($nom)) {
+                $errors['nom'] = 'Insérer votre nom';
+            }
+            
+            if(empty($prenom)) {
+                $errors['prenom'] = 'Insérer votre prénom';
+            }
+            
+            if(empty($sujet)) {
+                $errors['sujet'] = 'Veulliez préciser le sujet';
+            }
+            
+            if(empty($email)) {
+                $errors['email'] = 'Insérer votre adresse e-mail';
+            }
+            
+            if(empty($message)) {
+                $errors['message'] = 'Insérer votre message';
+            }
+            
+            if(empty($errors)) {
+                //si le formulaire est validé
+                $to = 'jonathan.n@codeur.online';
+                $headers ='From: "Nom_du_destinataire"<jonathan.n@codeur.online>'."\n";
+                $headers .='Content-Type: text/html; charset="utf-8"'."\n";
+                
+                $msg = 'nom: '.$nom;
+                $msg .= 'prenom: '.$prenom;
+                $msg .= 'sujet: '.$sujet;
+                $msg .= 'email: '.$email;
+                $msg .= 'message: '.$message;
+                
+            
+                if(mail($nom, $prenom, $sujet, $msg, $headers))
+                {
+                    echo '<script languag="javascript">alert("Le message a été correctement envoyé");</script>';
+                }
+                else
+                {
+                    echo '<script languag="javascript">alert("Le message n\'a pas pu être envoyé");</script>';
+                }			
+            }
+        }
     }
 }
+
